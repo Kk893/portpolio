5c1d1b17985b0eb10454a23fd48ecb70b4120d62
Kk893 <134618458+Kk893@users.noreply.github.com>
2025-01-31 16:10:40 +0530

    Update typhographic.js

--- JAVASCRIPT/typhographic.js
+++ JAVASCRIPT/typhographic.js
@@ -1,64 +1,30 @@
-function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function getRandomLetter() {
- var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
- return alphabet[rand(0,alphabet.length - 1)]
-}
-function getRandomWord(word) {
-  var text = word.innerHTML
-  
-  var finalWord = ''
-  for(var i=0;i<text.length;i++) {
-    finalWord += text[i] == ' ' ? ' ' : getRandomLetter()
-  }
- 
-  return finalWord
+// typhographic.js - Glitch Effect on Hover
+const word = document.querySelector(".glitch-text");
+const originalText = word.innerText;
+let isAnimating = false;
+
+function getRandomChar() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*";
+    return chars[Math.floor(Math.random() * chars.length)];
 }
 
-var word = document.querySelector('p')
-var interv = 'undefined'
-var canChange = false
-var globalCount = 0
-var count = 0
-var INITIAL_WORD = word.innerHTML;
-var isGoing = false
+function glitchEffect() {
+    if (isAnimating) return;
+    isAnimating = true;
+    let iterations = 0;
+    const interval = setInterval(() => {
+        word.innerText = originalText
+            .split("")
+            .map((char, i) => (Math.random() > 0.5 ? getRandomChar() : char))
+            .join("");
 
-function init() {
- if(isGoing) return;
- 
- isGoing = true
- var randomWord = getRandomWord(word)
- word.innerHTML = randomWord
-
- interv = setInterval(function() {
-  var finalWord = ''
-  for(var x=0;x<INITIAL_WORD.length;x++) {
-   if(x <= count && canChange) {
-    finalWord += INITIAL_WORD[x]
-   } else {
-    finalWord += getRandomLetter()
-   }
-  }
-  word.innerHTML = finalWord
-  if(canChange) {
-    count++
-  }
-  if(globalCount >= 20) {
-   canChange = true
-  }
-  if(count>=INITIAL_WORD.length) {
-   clearInterval(interv)
-   count = 0
-   canChange = false
-   globalCount = 0
-   isGoing = false
-  }
-  globalCount++
- },50)
- 
+        iterations++;
+        if (iterations > 10) {
+            clearInterval(interval);
+            word.innerText = originalText;
+            isAnimating = false;
+        }
+    }, 100);
 }
 
-
-
-word.addEventListener('mouseenter', init)
+word.addEventListener("mouseenter", glitchEffect);
